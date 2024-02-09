e6538a27421b3ba858514eaeabea13305c3c9dab
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2024-02-09 18:33:48 +0100

    Create routes for some pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App bg-blue-500 text-white p-4">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to our site!</p>
+    </div>
+  );
+}
+
+export default Home;
